1ad0659a96b92cabea00b668a80f01a220d5583e
paulsohier <paul999@phpbb.com>
2014-11-08 15:23:37 +0100

    Add first templates

--- event/shoutbox_listener.php
+++ event/shoutbox_listener.php
@@ -4,26 +4,43 @@
  * Ajax Shoutbox extension for the phpBB Forum Software package.
  *
  * @copyright (c) 2014 Paul Sohier <http://www.ajax-shoutbox.com>
- * @license GNU General Public License, version 2 (GPL-2.0)
+ * @license       GNU General Public License, version 2 (GPL-2.0)
  *
  */
 namespace paul999\ajaxshoutbox\event;
 
+class shoutbox_listener implements \Symfony\Component\EventDispatcher\EventSubscriberInterface {
 
-class shoutbox_listener implements \Symfony\Component\EventDispatcher\EventSubscriberInterface{
-
-	/** @var \phpbb\user  */
+	/** @var \phpbb\user */
 	private $user;
+
+	/** @var \phpbb\template\template */
+	private $template;
 
 	/**
 	 * @param \phpbb\user $user
 	 */
-	public function __construct(\phpbb\user $user) {
-		$this->user = $user;
+	public function __construct(\phpbb\user $user, \phpbb\template\template $template) {
+		$this->user     = $user;
+		$this->template = $template;
 	}
-	static public function getSubscribedEvents()
-	{
+
+	static public function getSubscribedEvents() {
 		return array(
+			'core.index_modify_page_title' => 'index',
 		);
 	}
-} 
+
+	/**
+	 *
+	 */
+	public function index() {
+		$this->template->assign_vars(
+			array(
+				'S_AJAX_SHOUTBOX' => true,
+			)
+		);
+	}
+}
+
+
