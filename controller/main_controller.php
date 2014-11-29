1f5b579586c2501f28d543db304facbda1f17e32
paulsohier <paul999@phpbb.com>
2014-11-29 14:45:55 +0100

    fix cs

--- controller/main_controller.php
+++ controller/main_controller.php
@@ -337,8 +337,7 @@ class main_controller
 		$username = get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
 		$username = str_replace('./../../', generate_board_url() . '/', $username); // Fix paths
 		$username = str_replace('./../', generate_board_url() . '/', $username); // Fix paths
-
-
+		
 		return array(
 			'id'      => $row['shout_id'],
 			'user'    => $username,
