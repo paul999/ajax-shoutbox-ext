ca69cd4e9817962aed5e60d73e3c4443ecaffb64
Mauron <Mauron@users.noreply.github.com>
2015-01-17 16:21:55 +0100

    Update acp_ajax_shoutbox.php
    Updated.

--- language/it/acp_ajax_shoutbox.php
+++ language/it/acp_ajax_shoutbox.php
@@ -39,6 +39,13 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge(
 	$lang, array(
+		'ACP_AJAXSHOUTBOX_SETTINGS'     => 'Impostazioni bacheca AJAX',
+		'ACP_AJAXSHOUTBOX_SETTINGS_EXPLAIN'     => 'In questa pagina, è possibile cambiare le impostazioni specifiche per la bacheca.<br />
+			<br />Per abilitare gli utenti all’uso delle applicazioni per iOS e Android (al momento in fase BETA privata ma utilizzabile 
+			da chi abbia accesso alla beta, leggere <a href="http://www.ajax-shoutbox.com/viewtopic.php?f=2&t=9">qui</a> per ulteriori informazioni), 
+			è necessario creare un profilo su <a href="https://www.shoutbox-app.com/">www.shoutbox-app.com</a> ed aggiungere un nuovo forum. Sarà 
+			richiesto un codice di attivazione, mostrato qui in basso.',
+
 		'ACP_AJAXSHOUTBOX_PRUNE'        => 'Impostazioni cancellazione automatica',
 		'AJAXSHOUTBOX_ENABLE_PRUNE'     => 'Abilita cancellazione automatica dei messaggi',
 		'AJAXSHOUTBOX_PRUNE_DAYS'       => 'Cancella automaticamente messaggi più vecchi di',
