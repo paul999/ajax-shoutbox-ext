9f9c648a27e61476e09e62a6962b76e75dfe9f59
paulsohier <paul999@phpbb.com>
2015-01-25 18:36:03 +0100

    Oops, I failed.

--- language/fr/acp_ajax_shoutbox.php
+++ language/fr/acp_ajax_shoutbox.php
@@ -61,7 +61,7 @@ $lang = array_merge(
 		'ACP_AJAXSHOUTBOX_CON_KEY_PUSH' => 'ID Connection',
 		'ACP_AJAXSHOUTBOX_CON_KEY_PUSH_EXPLAIN' => 'Vous recevrez cette clé après avoir ajouter votre forum sur www.shoutbox-app.com<br />Vos utilisateurs utiliseront cet ID pour trouver votre plateforme dans l’application mobile.',
 
-		'AJAXSHOUTBOX_PUSH_DISABLED'            => 'Push functionality disabled',
-		'AJAXSHOUTBOX_PUSH_DISABLED_EXPLAIN'    => 'The Push functionality is, by default, disabled. If you want to use this functionality add the following line to config.php: ',
+		'ACP_AJAXSHOUTBOX_PUSH_DISABLED'            => 'Push functionality disabled',
+		'ACP_AJAXSHOUTBOX_PUSH_DISABLED_EXPLAIN'    => 'The Push functionality is, by default, disabled. If you want to use this functionality add the following line to config.php: ',
 	)
 );
