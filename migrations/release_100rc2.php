a3a43453e51769500bd1d7c0cdfcf836f35cbfdf
Paul Sohier <paul999@phpbb.com>
2015-05-03 23:47:27 +0200

    Change config parameter for version name.

--- migrations/release_100rc2.php
+++ migrations/release_100rc2.php
@@ -23,7 +23,7 @@ class release_100rc2 extends migration
 	public function update_data()
 	{
 		return array(
-			array('config.update', array('profile_guestbook_version', '1.0.0-RC2'))
+			array('config.update', array('ajaxshoutbox_version', '1.0.0-RC2'))
 		);
 	}
 }
