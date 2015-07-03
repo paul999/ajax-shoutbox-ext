b33dd3dc89089f7235230b0e83c48b588f515fb4
Paul Sohier <paul999@phpbb.com>
2015-07-03 11:23:12 +0200

    Fix build.

--- travis/prepare-phpbb.sh
+++ travis/prepare-phpbb.sh
@@ -21,4 +21,4 @@ cp -R . ../../tmp
 cd ../../
 
 # Clone phpBB
-git clone --depth=1 "git://github.com/paul999/phpbb.git" "phpBB3" --branch=$BRANCH
+git clone --depth=1 "git://github.com/phpbb/phpbb.git" "phpBB3" --branch=$BRANCH
