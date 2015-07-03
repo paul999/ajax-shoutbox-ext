8ef328340878c761ca6114aba907940c0dc56e5e
Paul Sohier <paul999@phpbb.com>
2015-07-03 16:11:38 +0200

    Submit form to save

--- tests/functional/ucp_profile_test.php
+++ tests/functional/ucp_profile_test.php
@@ -26,7 +26,13 @@ class ucp_profile_test extends \phpbb_functional_test_case{
         $this->add_lang_ext('paul999/ajaxshoutbox', 'ajax_shoutbox');
         $this->login();
 
+        /** @var \Symfony\Component\DomCrawler\Crawler $crawler */
         $crawler = self::request('GET', 'ucp.php?i=ucp_prefs&mode=personal');
         $this->assertContains($this->lang('AJAXSHOUTBOX_BOARD_DATE_FORMAT'), $crawler->filter('#ajaxshoutbox_format_label')->text());
+
+        $form = $crawler->selectButton($this->lang['SUBMIT'])->form();
+
+        self::$client->submit($form);
+        self::assert_response_html();
     }
 }
